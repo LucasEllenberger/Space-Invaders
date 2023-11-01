630b8f461fe34f722616a9c8d2611ab1513760ec
Lucas Ellenberger <lucas.me02@gmail.com>
2023-11-01 18:09:29 +0100

    fixed attribute values

--- src/tp1/logic/gameobjects/Attributes.java
+++ src/tp1/logic/gameobjects/Attributes.java
@@ -6,8 +6,8 @@ package tp1.logic.gameobjects;
 
 public enum  Attributes {
 	Player(0, 1, 3),
-	RegularAlien(5, 0, 1),
-	DestroyerAlien(10, 1, 2),
+	RegularAlien(5, 0, 2),
+	DestroyerAlien(10, 1, 1),
 	Ufo(25, 0, 1);
 	
 	public final int points;
